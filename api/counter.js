bd63f0c81771b56e83078cff023a5c519e71a501
oroFly <31t3rn@gmail.com>
2024-11-29 14:40:13 +0100

    Update counter.js

--- api/counter.js
+++ api/counter.js
@@ -1,29 +1,19 @@
-const express = require('express');
 const fs = require('fs');
-const cors = require('cors');
-
-const app = express();
 const path = './counter.json';
 
-// Enable CORS
-app.use(cors());
-
-// Read counter from file or initialize it
 let counter = 0;
 if (fs.existsSync(path)) {
-  counter = JSON.parse(fs.readFileSync(path)).counter || 0;
+    counter = JSON.parse(fs.readFileSync(path)).counter || 0;
 }
 
-// Route to get the current counter
-app.get('/counter', (req, res) => {
-  res.json({ counter });
-});
-
-// Route to increment the counter
-app.post('/counter', (req, res) => {
-  counter++;
-  fs.writeFileSync(path, JSON.stringify({ counter }));
-  res.json({ counter });
-});
-
-module.exports = app; // Export the app instance
+module.exports = (req, res) => {
+    if (req.method === 'GET') {
+        res.json({ counter });
+    } else if (req.method === 'POST') {
+        counter++;
+        fs.writeFileSync(path, JSON.stringify({ counter }));
+        res.json({ counter });
+    } else {
+        res.status(405).send('Method Not Allowed');
+    }
+};
