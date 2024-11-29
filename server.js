55addc634790ef8a7d5bf0810eeb79d48b4d3012
oroFly <31t3rn@gmail.com>
2024-11-29 15:03:33 +0100

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const app = require('./counter'); // Import the app from counter.js
-
-// Start the server
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
-
