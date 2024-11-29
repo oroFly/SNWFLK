6a3f583a2098e1c1f2553c323ac7d22f4a9ab665
oroFly <31t3rn@gmail.com>
2024-11-29 14:57:31 +0100

    Update counter.js

--- api/counter.js
+++ api/counter.js
@@ -1,19 +1,15 @@
-const fs = require('fs');
-const path = './counter.json';
-
-let counter = 0;
-if (fs.existsSync(path)) {
-    counter = JSON.parse(fs.readFileSync(path)).counter || 0;
-}
+// api/counter.js
+let count = 0;
 
 module.exports = (req, res) => {
-    if (req.method === 'GET') {
-        res.json({ counter });
-    } else if (req.method === 'POST') {
-        counter++;
-        fs.writeFileSync(path, JSON.stringify({ counter }));
-        res.json({ counter });
-    } else {
-        res.status(405).send('Method Not Allowed');
-    }
+  if (req.method === 'GET') {
+    // Get the current counter value
+    return res.status(200).json({ counter: count });
+  } else if (req.method === 'POST') {
+    // Increase the counter when POST request is made
+    count++;
+    return res.status(200).json({ counter: count });
+  } else {
+    return res.status(405).json({ error: 'Method Not Allowed' });
+  }
 };
